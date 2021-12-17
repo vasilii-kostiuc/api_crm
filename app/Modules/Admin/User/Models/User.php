a1d7ea61bcef32c2c21bc67c37c3a9fc8fcf0528
vasile.costiuc <vasile.costiuc@it-lab.md>
2021-12-18 00:54:49 +0200

    Web Autorization

---
+++ app/Modules/Admin/User/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Modules\Admin\User\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as AuthUser;
+use Laravel\Passport\HasApiTokens;
+
+class User extends AuthUser
+{
+    use HasFactory, HasApiTokens;
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+        'phone',
+        'status'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+}
